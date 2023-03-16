ad446223d9631624f9069b1c611cdebecf5b59e8
Hye Hyun <Hye.Hyun@edu.sait.ca>
2023-03-16 00:15:04 -0600

    changes made

---
+++ Data/airport.cs
@@ -0,0 +1,10 @@
+using System.IO;
+
+namespace MauiApp1.Data
+{
+    public class Airport
+    {
+        public string City { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Data/airportmanager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Data
+{
+    public class AirportManager
+    {
+        public static List<Airport> GetAirports()
+        {
+            var airports = new List<Airport>();
+
+            using (var reader = new StreamReader(@"C:\Users\mac_h\source\repos\monkeyV10\mauiassignmentfinal\Data\airports.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    airports.Add(new Airport { City = values[0], Name = values[1] });
+                }
+            }
+
+            return airports;
+        }
+
+        public static List<string> GetCities()
+        {
+            var cities = new List<string>();
+            var airports = GetAirports();
+
+            foreach (var airport in airports)
+            {
+                if (!cities.Contains(airport.City))
+                {
+                    cities.Add(airport.City);
+                }
+            }
+
+            return cities;
+        }
+    }
+}
