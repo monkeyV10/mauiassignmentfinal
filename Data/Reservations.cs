c5b52cf2bb2d12967884381a428f06a2eff563b1
Hye Hyun <Hye.Hyun@edu.sait.ca>
2023-03-16 21:00:04 -0600

    yas

--- Data/Reservations.cs
+++ Data/Reservations.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiApp1.Data
 {
-    internal class Reservations
+    public class Reservation
     {
+        public string GenerateReservationCode()
+        {
+            const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Define the set of letters to use
+            const string digits = "0123456789"; // Define the set of digits to use
+            var random = new Random();
+            var reservationCode = $"{letters[random.Next(letters.Length)]}{random.Next(10)}{random.Next(10)}{random.Next(10)}{random.Next(10)}";
+
+            return reservationCode;
+        }
     }
+
 }
