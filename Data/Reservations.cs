fcf054c75e41b15c0cf076503878a4609c4d8b6b
Hye Hyun <Hye.Hyun@edu.sait.ca>
2023-03-19 03:18:17 -0600

    minor changes

--- Data/Reservations.cs
+++ Data/Reservations.cs
@@ -83,17 +83,7 @@ namespace MauiApp1.Data
         }
     }
 
-    public class ReservationData
-    {
-        public string SelectedFlightCode { get; set; }
-        public string SelectedAirline { get; set; }
-        public string SelectedDay { get; set; }
-        public string SelectedTime { get; set; }
-        public decimal Cost { get; set; }
-        public string Name { get; set; }
-        public string Citizenship { get; set; }
-        public string ReservationCode { get; set; }
-    }
+    
 
 }
 
---
+++ Data/reservationmanager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Data
+{
+    public class ReservationData
+    {
+        public string SelectedFlightCode { get; set; }
+        public string SelectedAirline { get; set; }
+        public string SelectedDay { get; set; }
+        public string SelectedTime { get; set; }
+        public decimal Cost { get; set; }
+        public string Name { get; set; }
+        public string Citizenship { get; set; }
+        public string ReservationCode { get; set; }
+    }
+}
